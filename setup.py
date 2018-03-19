c26566c78101d09df2d3ffe83d9774325245773e
Ian Woloschin <ian.woloschin@akamai.com>
2018-03-19 15:50:32 -0400

    Turn into system level 'executable' Add support for US Holidays

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name='street_sweeper',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    description='A simple tool to automatically generate recuring street sweeping reminders.',
+    url='https://github.com/iwoloschin/street_sweeper',
+    author='Ian Woloschin',
+    author_email='ian@woloschin.com',
+    license='MIT',
+    install_requires=[
+        'ics',
+    ],
+    entry_points={
+        'console_scripts': [
+            'street_sweeper=street_sweeper:main'
+        ],
+    },
+    zip_safe=False
+)
--- street_sweeper.py
+++ street_sweeper.py
@@ -1,47 +1,46 @@
-#!/usr/bin/env python3
-
-# Standard Python Libraries
+"""A simple utility to automatically generate street sweeping reminders."""
 import argparse
 import calendar
-from datetime import datetime, timedelta
+from datetime import datetime
+from datetime import timedelta
 from dateutil import tz
 
-# 3rd Party Libraries
 import holidays
 import ics
 
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
+
 days = {
-    'Sun' : calendar.SUNDAY,
-    'Mon' : calendar.MONDAY,
-    'Tue' : calendar.TUESDAY,
-    'Wed' : calendar.WEDNESDAY,
-    'Thu' : calendar.THURSDAY,
-    'Fri' : calendar.FRIDAY,
-    'Sat' : calendar.SATURDAY,
+    'Sun': calendar.SUNDAY,
+    'Mon': calendar.MONDAY,
+    'Tue': calendar.TUESDAY,
+    'Wed': calendar.WEDNESDAY,
+    'Thu': calendar.THURSDAY,
+    'Fri': calendar.FRIDAY,
+    'Sat': calendar.SATURDAY,
 }
 
-def generate_dates(year, week_parity, street_sweeping_day):
-    # Generate a list of street sweeping dates
 
-    # Street sweeping in Somerville goes April 1st till December 31st
-    starting_month = 4
-    ending_month = 12
-
-    est_edt = tz.tzlocal()
+def generate_dates(args, week_parity, street_sweeping_day):
+    """Return a list of street sweeping dates."""
     cal = calendar.Calendar()
     street_sweeping_dates = []
 
-    for month_number in range(starting_month, ending_month + 1):
-        # print(cal.monthdayscalendar(2016, month))
-        month = cal.monthdayscalendar(year, month_number)
+    for month_number in range(args.first, args.last + 1):
+        month = cal.monthdayscalendar(args.year, month_number)
         month_dates = []
         for week in month:
             for day in week:
                 if day:
                     if week.index(day) == days[street_sweeping_day]:
-                        event = datetime(year, month_number, day, hour=8, tzinfo=est_edt)
+                        event = datetime(
+                            args.year, month_number, day, hour=args.start, tzinfo=tz.tzlocal())
                         month_dates.append(event)
-
 
         while len(month_dates) > 4:
             month_dates.pop()
@@ -55,14 +54,16 @@ def generate_dates(year, week_parity, street_sweeping_day):
 
         street_sweeping_dates.append(month_dates)
 
-    # for month in street_sweeping_dates:
-    #     for day in month:
-    #         print(day)
+    for month in street_sweeping_dates:
+        for event in month:
+            if event.date() in holidays.UnitedStates():
+                month.remove(event)
 
     return street_sweeping_dates
 
-def generate_calendar(year, week_parity, street_sweeping_day, dates):
 
+def generate_calendar(args, week_parity, street_sweeping_day, dates):
+    """Generate an ICS Calendar and save to disk."""
     cal = ics.Calendar()
 
     for month in dates:
@@ -70,40 +71,51 @@ def generate_calendar(year, week_parity, street_sweeping_day, dates):
             event = ics.Event()
             event.name = 'Street Sweeping'
             event.begin = day
-            event.end = day + timedelta(hours=4)
+            event.end = day + timedelta(hours=(args.end - args.start))
             cal.events.append(event)
 
-    # for event in cal.events:
-    #     print(event)
-
-    with open(str(year) + '_' + week_parity + '_' +
-        street_sweeping_day + '_street_sweeping.ics', 'w') as ics_file:
+    filename = '{}_{}_{}_street_sweeping.ics'.format(
+        str(args.year), week_parity, street_sweeping_day)
+    with open(filename, 'w') as ics_file:
         ics_file.writelines(cal)
 
-def parse_args():
 
+def parse_args():
+    """Parse CLI arguments and provide sane defaults."""
     parser = argparse.ArgumentParser(description='Generate street sweeping calendar events')
-    parser.add_argument('-y', '--year', default=2016)
-    parser.add_argument('-w', '--week', help='1st & 3rd (Odd) or 2nd & 4th (Even) Weeks?',  nargs='+',
+    parser.add_argument('-y', '--year', default=datetime.now().year)
+    parser.add_argument(
+        '-w', '--week', help='1st & 3rd (Odd) or 2nd & 4th (Even) Weeks?',  nargs='+',
         choices=['odd', 'even'], default=['odd', 'even'])
-    parser.add_argument('-d', '--day', help='Day of street sweeping',  nargs='+',
+    parser.add_argument(
+        '-d', '--day', help='Day of street sweeping',  nargs='+',
         choices=calendar.day_abbr[0:5], default=calendar.day_abbr[0:5])
-    parser.add_argument('-s', '--start', help='Street sweeping start time (24 hr)', default=8)
-    parser.add_argument('-e', '--end', help='Street sweeping end time (24 hr)', default=12)
+    parser.add_argument(
+        '-s', '--start', default=8, choices=range(24),
+        help='Street sweeping start time (24 hr)')
+    parser.add_argument(
+        '-e', '--end', default=12, choices=range(24),
+        help='Street sweeping end time (24 hr)')
+    parser.add_argument(
+        '-f', '--first', default=4, choices=range(1, 13),
+        help='First month of the year when street sweeping begins')
+    parser.add_argument(
+        '-l', '--last', default=12, choices=range(1, 13),
+        help='Last month of the year with street sweeping')
+    parser.add_argument(
+        '-v', '--version', help='Print version of street_sweeper', action='store_true')
 
     return parser.parse_args()
 
-def main():
 
+def main():
+    """Main function for street_sweeper."""
     args = parse_args()
+
+    if args.version:
+        print(__version__)
 
     for week in args.week:
         for day in args.day:
-            dates = generate_dates(args.year, week, day)
-            generate_calendar(args.year, week, day, dates)
-
-    # dates = generate_dates(args)
-    # generate_calendar(args, dates)
-
-if __name__ == "__main__":
-    main()
+            dates = generate_dates(args, week, day)
+            generate_calendar(args, week, day, dates)
